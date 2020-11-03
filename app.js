45c99410851a5ff108427789ca8aceb8329b6888
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-03 13:18:44 +0530

    Added Payment GateWay Styles

---
+++ app.js
@@ -0,0 +1,23 @@
+myButton = document.getElementById("myBtn");
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+const wrapper = document.querySelector('.header-extradiv');
+const wrapperDesigners = document.querySelector('#clientdiv');
+wrapper.addEventListener('click', (event) => {
+    window.location.href = "#clientdiv"
+});
+wrapperDesigners.addEventListener('click', function (event) {
+    window.location.href = "checkout.html";
+});
