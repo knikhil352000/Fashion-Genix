2eb065325e1b4a659860616a0b386f08cbed1988
Kumar Nikhil <knikhil352000@gmail.com>
2020-11-03 13:53:20 +0530

    Added Favicon

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 const wrapper = document.querySelector('.header-extradiv');
-const wrapperDesigners = document.querySelector('#clientdiv');
+const designers = document.querySelector('#clientdiv')
+const wrapperDesigners = document.querySelector('#pricingdiv');
 wrapper.addEventListener('click', (event) => {
     window.location.href = "#clientdiv"
 });
 wrapperDesigners.addEventListener('click', function (event) {
     window.location.href = "checkout.html";
+});
+designers.addEventListener('click', function (event) {
+    window.location.href = "#pricingdiv";
 });
